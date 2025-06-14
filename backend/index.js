69455148c6574a067dcd30a113400527462104c7
Rafael <rafael_guliev94@list.ru>
2025-06-14 18:40:37 +0300

    update fixes

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ const corsOptions = {
   //     callback(new Error("Not allowed by CORS"));
   //   }
   // },
-  origin: ["https://voice-app-xi.vercel.app/", "http://localhost:5173"],
+  origin: ["https://voice-app-xi.vercel.app", "https://voice-app-xlw7.onrender.com/create-room", "http://localhost:5173"],
   methods: ["GET", "POST", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
